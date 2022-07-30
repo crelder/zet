9e7724cfafcce703f7d78919044c4bf426be440a
Christian Ziegler <christian.ziegler@real-digital.de>
2022-07-30 08:45:16 +0200

    Fix pipeline error

--- pkg/imports/imports_test.go
+++ pkg/imports/imports_test.go
@@ -14,13 +14,13 @@ func TestCreateImports(t *testing.T) {
 	if err != nil {
 		t.Errorf("could not get the current working dir")
 	}
-	var pathTestRepo = wd + "/testdata/zettelkasten/"
+	var pathTestRepo = wd + "/testdata/zettelkasten"
 	p := parse.New()
 	r := repo.New(pathTestRepo, p)
 	importer := New(p, r)
 
 	// Rebuild a clean state of the zettel folder
-	err2 := os.RemoveAll(pathTestRepo + "zettel/")
+	err2 := os.RemoveAll(pathTestRepo + "/zettel")
 	if err2 != nil {
 		t.Errorf("could not remove zettel folder for recreating it")
 	}
@@ -30,11 +30,12 @@ func TestCreateImports(t *testing.T) {
 11.8.21
 
 Some thought...`
-	err3 := os.Mkdir(pathTestRepo+"zettel", 0755)
+	err3 := os.Mkdir(pathTestRepo+"/zettel", 0755)
 	if err3 != nil {
 		t.Errorf("could not create zettel folder: %v", err3)
 	}
-	err4 := os.WriteFile(pathTestRepo+"zettel/"+"211005p - Post-capitalism.txt", []byte(zettel), 0755)
+	fnName := pathTestRepo + "/zettel/" + "211005p - Post-capitalism.txt"
+	err4 := os.WriteFile(fnName, []byte(zettel), 0755)
 	if err4 != nil {
 		t.Errorf("could not write zettel file: %v", err4)
 	}
@@ -51,7 +52,7 @@ Some thought...`
 
 	// Assert
 	if err != nil {
-		t.Errorf("error creating imports. path = %q", err)
+		t.Errorf("error creating import: %v", err)
 	}
 	if n != 2 {
 		t.Errorf("Imported %v files, should have imported 2", n)
@@ -61,13 +62,14 @@ Some thought...`
 		// Since ID 211005p already exists in the zettelkasten and therefore building the id from the date + the first
 		// letter of the first keyword doesn't work (=211005p), use the letter from the second keyword which will result
 		// in 211005g.
-		"zettel/211005g - Prozess, Glück, Enttäuschung, Hoffnung - Paul Watzlawick, Anleitung zum Unglücklich sein.txt",
+		"211005g - Prozess, Glück, Enttäuschung, Hoffnung - Paul Watzlawick, Anleitung zum Unglücklich sein.txt",
 		// 211005p is already in the zettelkasten. Since this zettel has just one keyword, it should try building the
 		// id with letters from a to z from the alphabet.
-		"zettel/211005a - Probleme - Dave Cheney.txt",
+		"211005a - Probleme - Dave Cheney.txt",
 	}
+	pathZettelTestRepo := pathTestRepo + "/zettel/"
 	for _, tc := range testcases {
-		if _, err := os.Stat(pathTestRepo + tc); err != nil {
+		if _, err := os.Stat(pathZettelTestRepo + tc); err != nil {
 			t.Errorf("File was not created: %v", tc)
 		}
 	}
