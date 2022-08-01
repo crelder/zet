d54b01a0dd9b5cffc5ce1a1e91528ff52879ff46
Christian Ziegler <christian.ziegler@real-digital.de>
2022-08-01 23:05:13 +0200

    Implement error return

--- cmd/cobra/cmd/validate.go
+++ cmd/cobra/cmd/validate.go
@@ -20,8 +20,9 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
 		fmt.Println("validate called")
+		return fmt.Errorf("my error")
 	},
 }
 
