7c015e28f8e7684d8089aa4762e06425ad7bfa66
Christian Ziegler <christian.ziegler@real-digital.de>
2022-08-01 22:59:51 +0200

    Move to correct folder

--- cmd/cobra/main.go
+++ cmd/cobra/main.go
@@ -4,7 +4,9 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/crelder/zet/zk/cmd"
+import (
+	"github.com/crelder/zet/cmd/cobra/cmd"
+)
 
 func main() {
 	cmd.Execute()
