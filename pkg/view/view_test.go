3053063c5e7c4e3312b03f14097b7bce02dc6067
Christian Ziegler <christian.ziegler@real-digital.de>
2022-08-03 12:31:43 +0200

    Implement testcase for info

--- pkg/view/view_test.go
+++ pkg/view/view_test.go
@@ -3,7 +3,9 @@ package view
 import (
 	"github.com/crelder/zet/pkg/parse"
 	"github.com/crelder/zet/pkg/transport/repo"
+	"github.com/google/go-cmp/cmp"
 	"os"
+	"path"
 	"testing"
 )
 
@@ -52,6 +54,103 @@ func TestCreateIndexViews(t *testing.T) {
 		if _, err := os.Stat(pathTestRepo + "/" + tc); err != nil {
 			t.Errorf("Symlink was not created: %+v, ", tc)
 		}
+	}
+}
+
+func TestCreateInfo(t *testing.T) {
+	// Arrange
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Errorf("could not get the current working dir")
+	}
+	var pathTestRepo = wd + "/testdata/zettelkasten2"
+	parser := parse.New()
+	r := repo.New(pathTestRepo, parser)
+	viewer := New(r, r, r)
+
+	// Remove this directory, which might got created in a previous test
+	infoPath := pathTestRepo + "/INFO"
+	clearPath(infoPath)
+
+	// Act
+	err = viewer.CreateViews()
+	if err != nil {
+		t.Errorf("Could not generate views: %v", err)
+	}
+
+	// Assert
+	type wantedFiles struct {
+		filename string
+		content  string
+	}
+
+	want := []wantedFiles{
+		{
+			"ids.csv",
+			"170224a;1\n180522a;2\n190119e;1",
+		},
+		{
+			"keywords.csv",
+			"Complexity;3\nInterface;1\nPolymorphism;1\nTesting;1",
+		},
+
+		{
+			"context.csv",
+			"GopherCon EU 2022;2",
+		},
+		{
+			"references.csv",
+			"clausen2021;1",
+		},
+		{
+			"bibkeys.csv",
+			"kernighan1999;1\nsedgewick2011;1",
+		},
+		{
+			"unlinked.csv",
+			"180522a;1", // 190119e is also unliked, but is references in the index.
+		},
+		{
+			"unindexed.csv",
+			// There is no index entry for this zettel,
+			//although a chain of thoughts with the maximal length of one branch of 2 exists.
+			"170224a;2",
+		},
+		{
+			"links.csv",
+			"170224a;1\n190119d;1",
+		},
+		//{
+		//	"date.csv",
+		//	"Komplexität;2\nPresntation;1",
+		//},
+		//{
+		//	"index.csv",
+		//	"Komplexität;2\nProgrammieren, Objektorientiert;1",
+		//},
+		//{
+		//	"unrelated.csv",
+		//	"TODO",
+		//},
+	}
+
+	dir, err := os.ReadDir(infoPath)
+	if err != nil {
+		t.Errorf("error reading dir %v: %v", infoPath, err)
+	}
+
+	// map[filename]content
+	got := make(map[string]string)
+	for _, dirEntry := range dir {
+		file, err := os.ReadFile(path.Join(infoPath, dirEntry.Name()))
+		if err != nil {
+			t.Errorf("error reading file %v: %v", path.Join(infoPath, dirEntry.Name()), err)
+		}
+		got[dirEntry.Name()] = string(file)
+	}
+
+	if diff := cmp.Diff(got, want); diff != "" {
+		t.Errorf(diff)
 	}
 }
 
