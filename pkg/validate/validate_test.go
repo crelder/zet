a1eb2bd2c031b132253c777ec277dcc56a896048
crelder <22595757+crelder@users.noreply.github.com>
2022-12-23 01:03:53 +0100

    Prepare testcases

--- pkg/validate/validate_test.go
+++ pkg/validate/validate_test.go
@@ -32,62 +32,15 @@ func TestValidate(t *testing.T) {
 	}
 
 	want := map[string]bool{
-		"index: link to id 180317q not existing": true,
-		"reference: missing bibkey \"knut2012\"": true,
-		//"zettel: more than one predecessor: 170327f": true,  // Need to throw a parse error
-		"zettel: link to id 170311f not existing": true,
-		"zettel: id 180112a not unique":           true,
+		"zettel: more than one predecessor: 170327f":   true,
+		"zettel: link to id 160122e not existing":      true,
+		"zettel: id 180112a not unique":                true,
+		"zettel: cannot parse filename 'noId.txt'":     true,
+		"index: could not parse line 'Water::170312w'": true,
+		"reference: missing bibkey \"pike1989\"":       true,
 	}
 
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
-	}
-}
-
-func TestValidateParsingIdError(t *testing.T) {
-	// Arrange
-	wd, err := os.Getwd()
-	if err != nil {
-		t.Errorf("could not get the current working dir")
-	}
-	var pathTestRepo = wd + "/testdata/zettelkasten2"
-	parser := parse.New()
-	r := fs.New(pathTestRepo, parser)
-	validator := New(r)
-
-	// Act
-	_, err = validator.Val()
-
-	// Assert
-	if err == nil {
-		t.Errorf("Should have received error 'parse Filename: could not parse id from filename \"noId - Something.txt\"', but didn't")
-	}
-
-	if err != nil && err.Error() != "parse Filename: could not parse id from filename \"noId - Something.txt\"" {
-		t.Errorf("received err string: %v. But expected: not unique id 1801112a", err.Error())
-	}
-}
-
-func TestValidateParsingIndexError(t *testing.T) {
-	// Arrange
-	wd, err := os.Getwd()
-	if err != nil {
-		t.Errorf("could not get the current working dir")
-	}
-	var pathTestRepo = wd + "/testdata/zettelkasten3"
-	parser := parse.New()
-	r := fs.New(pathTestRepo, parser)
-	validator := New(r)
-
-	// Act
-	_, err = validator.Val()
-
-	// Assert
-	if err == nil {
-		t.Errorf("Should have received error 'parse Filename: could not parse id from filename \"noId - Something.txt\"', but didn't")
-	}
-
-	if err != nil && err.Error() != "index: could not parse line \"Water::170312w\"" {
-		t.Errorf("received err string: %v. But expected: index: could not parse line \"Water::170312w\"", err.Error())
 	}
 }
